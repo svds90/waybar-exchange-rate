73ec9ce03d82e9a87625347ac09ba96c1360def1
svds90 <svds22090@gmail.com>
2024-09-28 16:17:21 +0300

    upd

--- exchange-rate.sh
+++ exchange-rate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 SYMBOL=""
-URL="https://api.binance.com/api/v3/ticker/price?symbol=$SYMBOL"
+URL="https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=$SYMBOL"
 response=$(curl -s "$URL")
-price=$(echo "$response" | jq -r '.price')
+price=$(echo "$response" | jq -r '.data.price')
 
 if [[ -n "$price" ]]; then
 	formatted_price=$(printf "%.3f" "$price")
-    echo " $formatted_price"
+    echo "$formatted_price"
 else
     echo "404"
 fi
